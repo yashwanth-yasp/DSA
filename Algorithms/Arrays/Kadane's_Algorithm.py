83c6126f0b81cae4e2b936e07abc568cfee93cbf
Yashwanth A Doddegowda <ya64597@gmail.com>
2023-07-30 14:47:59 +0000

    kadane's algo update

---
+++ Algorithms/Arrays/Kadane's_Algorithm.py
@@ -0,0 +1,17 @@
+
+def kadanes_algo(arr):
+    curr_sum = 0
+    max_sum = arr[0]
+
+    for i in arr:
+        curr_sum = max(curr_sum, 0) + i
+        max_sum = max(curr_sum, max_sum)
+    
+    return max_sum
+
+
+line = list(map(int, input()))
+print(f"The max sum is {kadanes_algo(line)}")
+
+
+
